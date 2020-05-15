4297825bf05b86f2d6aae68e01770dd7e853acce
github username <justinzhao265@gmail.com>
2020-05-15 00:02:34 -0400

    weather api

--- src/index.js
+++ src/index.js
@@ -6,14 +6,24 @@ function logTimeWeather(input){
   geocodeAPICall(input)
 }
 
-async function geocodeAPICall(input){
-  let response = await fetch(`https://api.opencagedata.com/geocode/v1/json?q=${input}&key=` + process.env.REACT_APP_OCD_API_KEY).then(resp => resp.json())
-  let timezoneInfo = await response.results[0].annotations.timezone.name
+function logTime(timezoneInput){
   let now = moment(new Date())
-  console.log('Current Time: ', now.tz(timezoneInfo))
+  console.log('Current Time: ', now.tz(timezoneInput).format('MMMM Do YYYY, h:mm:ss a'))
 }
 
+async function weatherAPICall(latlon){
+  let weatherResp = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latlon.lat}&lon=${latlon.lng}&units=imperial&appid=` + process.env.REACT_APP_OWM_API_KEY).then(data => data.json())
+  let weatherInfo = await weatherResp.main.temp
+  console.log('Temp: ', Math.round(weatherInfo) + '\xB0F' )
+}
 
+async function geocodeAPICall(input){
+  const response = await fetch(`https://api.opencagedata.com/geocode/v1/json?q=${input}&key=` + process.env.REACT_APP_OCD_API_KEY).then(resp => resp.json())
+  let timezoneInfo = await response.results[0].annotations.timezone.name
+  let latlon = await response.results[0].geometry
+  logTime(timezoneInfo)
+  weatherAPICall(latlon)
+}
 
 logTimeWeather(
   'new york, ny 10004'
